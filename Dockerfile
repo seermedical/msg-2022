ae7cd1f68ac9ed288476321cee124ae7df8d7e8a
Ronny Restrepo <ronny.restrepo@seermedical.com>
2022-07-06 17:08:58 +1000

    FEAT: python dependencies for parquet

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM python:3.8.11
 WORKDIR /app
-# XXX: TODO: use bucket that will actually be run in production.
-ENV DATA_BUCKET_FILE="XXX"
 
-# POTENTIALLY INSTALL PYTHON DEPENDENCIES IF YOU HAVE THEM
-# COPY requirements.txt ./
-# RUN pip install -r requirements.txt
+# OPTIONALLY INSTALL PYTHON DEPENDENCIES IF YOU HAVE THEM
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
 
 # COPY WHATEVER OTHER SCRIPTS YOU MAY NEED
 # COPY scriptA.py scriptB.py scriptC.py ./
--- dummy.py
+++ dummy.py
@@ -1,41 +1,38 @@
 import os
 from pathlib import Path
-# import pandas as pd
-# import numpy as np
+import pandas as pd
+import numpy as np
 
-# Python Dependencies
-# pyarrow
-# fastparquet
 
 # SETTINGS
 DATA_DIR = Path("/data/test/") # Location of input test data
-PREDICTIONS_FILEPATH = "./predictions.csv"  # Output file
+PREDICTIONS_FILEPATH = "./predictions.csv" # Output file.
 
 
 # GET LIST OF ALL THE PARQUET FILES TO DO PREDICTIONS FOR
+print("Getting list of files to run predictions on.")
 test_files = []
 for patient in os.listdir(DATA_DIR):
     for session in os.listdir(DATA_DIR/patient):
         for filename in os.listdir(DATA_DIR/patient/session):
             test_files.append(Path(patient)/session/filename)
 
-print("FILES")
-for file in test_files:
-    print(file)
+# CREATE PREDICTIONS
+print("Creating predictions.")
+np.random.seed(1)
+predictions = []
+for filepath in test_files:
+    # Load up the input data
+    X = pd.read_parquet(DATA_DIR/filepath)
 
-# # CREATE PREDICTIONS
-# np.random.seed(1)
-# predictions = []
-# for filepath in test_files:
-#     # Load up the input data
-#     X = pd.read_parquet(DATA_DIR/filepath)
+    # Feed the input to some machine learning model, and get a prediction.
+    print("Predicting on file {filepath}")
+    prediction = np.random.rand()
 
-#     # Feed the input to some machine learning model, and get a prediction.
-#     prediction = np.random.rand()
+    # Append to your predictions (along with the file it corresponds with)
+    predictions.append([str(filepath), prediction])
 
-#     # Append to your predictions (along with the file it corresponds with)
-#     predictions.append([str(filepath), prediction])
-
-# # SAVE PREDICTIONS TO A CSV FILE
-# predictions = pd.DataFrame(predictions, columns=["filepath", "prediction"])
-# predictions.to_csv(PREDICTIONS_FILEPATH)
+# SAVE PREDICTIONS TO A CSV FILE
+print("Saving predictions.")
+predictions = pd.DataFrame(predictions, columns=["filepath", "prediction"])
+predictions.to_csv(PREDICTIONS_FILEPATH)
