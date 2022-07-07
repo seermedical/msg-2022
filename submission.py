2804ad8979d0a667f6be778091d2b41fdcf169cc
Ronny Restrepo <ronny.restrepo@seermedical.com>
2022-07-07 10:59:46 +1000

    DOCS: submission py docstring

--- submission.py
+++ submission.py
@@ -1,3 +1,6 @@
+"""
+This is the main script that will create the predictions on input data and save a predictions file.
+"""
 import os
 from pathlib import Path
 import pandas as pd
@@ -9,7 +12,7 @@ DATA_DIR = Path("/data/test/") # Location of input test data
 PREDICTIONS_FILEPATH = "/predictions/predictions.csv" # Output file.
 
 
-# GET LIST OF ALL THE PARQUET FILES TO DO PREDICTIONS FOR
+# GET LIST OF ALL THE PARQUET FILES TO DO PREDICTIONS ON
 print("Getting list of files to run predictions on.")
 test_files = []
 for patient in os.listdir(DATA_DIR):
@@ -25,9 +28,13 @@ for filepath in test_files:
     # Load up the input data
     X = pd.read_parquet(DATA_DIR/filepath)
 
+    # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    # YOUR MAGIC SAUCE HERE
+    # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     # Feed the input to some machine learning model, and get a prediction.
     print(f"Predicting on file {filepath}")
     prediction = np.random.rand()
+    # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
     # Append to your predictions (along with the file it corresponds with)
     predictions.append([str(filepath), prediction])
