3801cc769cad689d9ad6bf9b18072dd029ca3a7d
ChangWeiTan <cwtan229@gmail.com>
2022-09-08 10:24:53 +1000

    update demo codes

--- examples/multirocket/submission_patient_specific.py
+++ examples/multirocket/submission_patient_specific.py
@@ -18,10 +18,6 @@ TEST_DATA_DIR = Path("/dataset/test/")  # Location of input test data
 PREDICTIONS_FILEPATH = "/submission/submission.csv"  # Output file.
 VERSION = "v0.1.0"  # Submission version. Optional and purely for logging purposes.
 
-lr_model = tf.keras.models.load_model(TRAINED_MODEL_DIR)
-with open(TRAINED_MODEL_DIR / "multirocket.pkl", "rb") as f:
-    multirocket = pickle.load(f)
-
 # GET LIST OF ALL THE PARQUET FILES TO DO PREDICTIONS ON
 print("Getting list of files to run predictions on.")
 test_files = []
@@ -29,31 +25,39 @@ for patient in os.listdir(TEST_DATA_DIR):
     for session in os.listdir(TEST_DATA_DIR / patient):
         for filename in os.listdir(TEST_DATA_DIR / patient / session):
             test_files.append(Path(patient) / session / filename)
-predictions = pd.DataFrame({"filepath": test_files})
-
-
-# CREATE PREDICTIONS
-def minirocket_transform(x):
-    return multirocket.transform(x.numpy())
-
-
-def tf_minirocket_transform(x):
-    return tf.py_function(minirocket_transform, [x], tf.float64)
-
+test_data = pd.DataFrame({"filepath": test_files})
+test_data["patient"] = test_data["filepath"].map(lambda x: str(x).split("/")[0])
 
 print("Creating predictions.")
 
-x_test = predictions["filepath"].map(lambda x: str(TEST_DATA_DIR / x))
+predictions = []
+for patient, group in test_data.groupby("patient"):
+    x_test = group["filepath"].map(lambda x: str(TEST_DATA_DIR / x))
+    lr_model = tf.keras.models.load_model(TRAINED_MODEL_DIR / patient)
+    with open(TRAINED_MODEL_DIR / patient / "multirocket.pkl", "rb") as f:
+        multirocket = pickle.load(f)
 
-print(x_test.shape)
-test_dataset = (
-    tf.data.Dataset.from_tensor_slices(x_test)
-        .map(tf_load_parquet)
-        .batch(32, drop_remainder=False)
-        .map(tf_minirocket_transform)
-)
-y_pred = lr_model.predict(test_dataset).ravel()
-predictions["prediction"] = y_pred
+
+    # CREATE PREDICTIONS
+    def multirocket_transform(x):
+        return multirocket.transform(x.numpy())
+
+
+    def tf_multirocket_transform(x):
+        return tf.py_function(multirocket_transform, [x], tf.float64)
+
+
+    test_dataset = (
+        tf.data.Dataset.from_tensor_slices(x_test)
+            .map(tf_load_parquet)
+            .batch(32, drop_remainder=False)
+            .map(tf_multirocket_transform)
+    )
+    y_pred = lr_model.predict(test_dataset).ravel()
+    group["prediction"] = y_pred
+    predictions.append(group[["filepath", "prediction"]])
+
+predictions = pd.concat(predictions)
 
 # SAVE PREDICTIONS TO A CSV FILE
 print("Saving predictions.")
