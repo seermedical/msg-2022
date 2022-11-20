6e016e5be8619951618140cd1d656a49023d6829
Duong Nhu <d.binhnhu@gmail.com>
2022-11-20 16:50:31 +1100

    added inception_time

---
+++ examples/inceptiontime/inception.py
@@ -0,0 +1,142 @@
+"""
+    This is an adaption of the InceptionTime: https://github.com/hfawaz/InceptionTime
+"""
+from tensorflow.keras.layers import (
+    Add,
+    BatchNormalization,
+    Conv1D,
+    Concatenate,
+    MaxPool1D,
+    GlobalAveragePooling1D,
+    Activation,
+    Dropout,
+    Layer,
+    AveragePooling1D,
+    SpatialDropout1D,
+    Flatten,
+    MaxPooling1D
+)
+from tensorflow.keras.regularizers import l2
+
+
+class Inception:
+    def __init__(
+        self,
+        use_bottleneck=True,
+        bottleneck_size=32,
+        kernel_size=40,
+        nb_filters=32,
+        depth=6,
+        use_residual=True,
+        increase_filters=False,
+        increase_factor=2,
+        l2_weight=0.0,
+        dropout_rate=0.0,
+        use_spatial_dropout=False,
+        spatial_dropout_rate=0.1
+    ):
+        self.use_bottleneck = use_bottleneck
+        self.bottleneck_size = bottleneck_size
+        self.kernel_size = kernel_size
+        self.nb_filters = nb_filters
+        self.depth = depth
+        self.use_residual = use_residual
+        self.increase_filters = increase_filters
+        self.increase_factor = increase_factor
+        self.l2_weight = l2_weight
+        self.dropout_rate = dropout_rate
+        self.use_spatial_dropout = use_spatial_dropout
+        self.spatial_dropout_rate = spatial_dropout_rate
+
+    def create_model(self, inputs):
+        x = inputs
+        input_res = inputs
+
+        bottle_size = self.bottleneck_size
+        nb_filters = self.nb_filters
+
+        for d in range(self.depth):
+            if self.increase_filters and d > 0 and d % 2 == 0:
+                bottle_size *= self.increase_factor
+                nb_filters *= self.increase_factor
+
+            x = self._inception_module(
+                x, bottleneck_size=bottle_size, nb_filters=nb_filters
+            )
+            if self.use_spatial_dropout:
+                x = SpatialDropout1D(self.spatial_dropout_rate)(x)
+
+            if self.use_residual and d % 3 == 2:
+                x = self._shortcut_layer(input_res, x)
+                input_res = x
+
+        gap_layer = GlobalAveragePooling1D()(x)
+        output = Dropout(self.dropout_rate)(gap_layer)
+
+        return output
+
+    def _inception_module(
+        self, input_tensor, bottleneck_size, nb_filters, stride=1, activation="linear"
+    ):
+        if self.use_bottleneck and int(input_tensor.shape[-1]) > 1:
+            input_inception = Conv1D(
+                filters=bottleneck_size,
+                kernel_size=1,
+                padding="same",
+                activation=activation,
+                use_bias=False,
+                kernel_regularizer=l2(self.l2_weight),
+            )(input_tensor)
+        else:
+            input_inception = input_tensor
+
+        kernel_size_s = [self.kernel_size // (2 ** i) for i in range(3)]
+
+        conv_list = []
+
+        for i in range(len(kernel_size_s)):
+            conv_list.append(
+                Conv1D(
+                    filters=nb_filters,
+                    kernel_size=kernel_size_s[i],
+                    strides=stride,
+                    padding="same",
+                    activation=activation,
+                    use_bias=False,
+                    kernel_regularizer=l2(self.l2_weight),
+                )(input_inception)
+            )
+
+        max_pool_1 = MaxPool1D(pool_size=3, strides=stride, padding="same")(
+            input_tensor
+        )
+
+        conv_6 = Conv1D(
+            filters=nb_filters,
+            kernel_size=1,
+            padding="same",
+            activation=activation,
+            use_bias=False,
+            kernel_regularizer=l2(self.l2_weight),
+        )(max_pool_1)
+
+        conv_list.append(conv_6)
+
+        x = Concatenate(axis=2)(conv_list)
+        x = BatchNormalization()(x)
+        x = Activation(activation="relu")(x)
+        return x
+
+    def _shortcut_layer(self, input_tensor, out_tensor):
+        shortcut_y = Conv1D(
+            filters=int(out_tensor.shape[-1]),
+            kernel_size=1,
+            padding="same",
+            use_bias=False,
+            kernel_regularizer=l2(self.l2_weight),
+        )(input_tensor)
+        shortcut_y = BatchNormalization()(shortcut_y)
+
+        x = Add()([shortcut_y, out_tensor])
+        x = Activation("relu")(x)
+        return x
---
+++ examples/inceptiontime/preprocess_data.py
@@ -0,0 +1,73 @@
+import os
+from argparse import ArgumentParser
+import numpy as np
+import pandas as pd
+import scipy.signal
+from p_tqdm import p_map
+
+
+SEED = 42
+
+
+def transform_data(file):
+    x = pd.read_parquet(file, engine="pyarrow")
+    x = x.fillna(0)
+    x = np.transpose(x.values.tolist())
+    f, t, Zxx = scipy.signal.stft(
+        x, fs=128, window="hann", nperseg=128 * 10, scaling="psd", return_onesided=False
+    )
+    x = np.swapaxes(Zxx, -1, 1)
+    x = x.real.astype(np.float64)
+
+    return x
+
+
+def preprocess(params) -> str:
+    file, save_path = params
+    x = transform_data(file)
+
+    processed_file = os.path.join(
+        save_path, "/".join(file.split("/")[-4:]).split(".")[0] + ".bin"
+    )
+    folder = os.path.dirname(processed_file)
+    try:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+    except Exception as e:
+        pass
+
+    x.tofile(processed_file)
+    return processed_file
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(
+        "--data-path", type=str, default="/dataset/train/", required=False
+    )
+
+    arg_parser.add_argument(
+        "--train-label",
+        type=str,
+        default="/dataset/train/train_labels.csv",
+        required=False,
+    )
+
+    arg_parser.add_argument(
+        "--preprocessed-path", type=str, default=None, required=True
+    )
+
+    args = arg_parser.parse_args()
+
+    train_labels = pd.read_csv(args.data_label)
+    train_labels["filepath"] = train_labels["filepath"].map(
+        lambda x: os.path.join(args.data_path, x)
+    )
+
+    p_map(
+        preprocess,
+        zip(
+            train_labels["filepath"],
+            [args.preprocessed_path] * train_labels.shape[0],
+        ),
+    )
